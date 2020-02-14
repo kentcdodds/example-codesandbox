f769fdaec3a521dd41b8ce95d84bce40ad817120
Kent C. Dodds <me+github@kentcdodds.com>
2020-02-14 14:00:18 -0700

    change title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './styles.css'
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello You!</h1>
+      <h1>Hello There!</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   )
